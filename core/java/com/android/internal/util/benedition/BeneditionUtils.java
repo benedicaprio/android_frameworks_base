ed0851acb65d91ed68b371e9eacdc5e95731567d
bigrushdog <randall.rushing@gmail.com>
2017-09-12 14:14:35 +0800

    Utils: Add api to take screenshots
    abc: extracted from DUI
    evo: add a public call to EvolutionUtils
    adapted for partial screenshot
    
    Change-Id: Ic1303dae913bbd22bd7bf98540b4b032eb06a91c

--- core/java/com/android/internal/util/benedition/BeneditionUtils.java
+++ core/java/com/android/internal/util/benedition/BeneditionUtils.java
@@ -18,6 +18,7 @@ package com.android.internal.util.benedition;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.fingerprint.FingerprintManager;
 import android.hardware.input.InputManager;
@@ -29,6 +30,8 @@ import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.SystemProperties;
+import android.view.IWindowManager;
+import android.view.WindowManagerGlobal;
 import android.view.InputDevice;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
@@ -46,6 +49,9 @@ import com.android.internal.statusbar.IStatusBarService;
  * Some custom utilities
  */
 public class BeneditionUtils {
+
+    public static final String INTENT_SCREENSHOT = "action_take_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
 
     // Check to see if device is WiFi only
     public static boolean isWifiOnly(Context context) {
@@ -150,6 +156,15 @@ public class BeneditionUtils {
                         InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
             }
         }, 20);
+    }
+
+    public static void takeScreenshot(boolean full) {
+        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
     private static final class FireActions {
