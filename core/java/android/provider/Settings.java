ddf64216ea3d602cd24ebf32f771d75f1de1e310
Anushek Prasal <anushekprasal@gmail.com>
2019-11-14 14:07:44 +0530

    base: Add refresh rate setting [1/2]
    Change-Id: I086c9053cc59f8c0c62cbbf72ad25e62b22db39b
    Signed-off-by: rituj <ritujbeniwal@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4737,6 +4737,11 @@ public final class Settings {
         private static final Validator NAVIGATION_HANDLE_WIDTH_VALIDATOR = ANY_INTEGER_VALIDATOR;
 
         /**
+         * @hide
+         */
+        public static final String REFRESH_RATE_SETTING = "refresh_rate_setting";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
